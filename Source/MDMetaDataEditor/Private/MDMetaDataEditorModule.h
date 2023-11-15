c1c9369a0e0b5f7ecaaed390c5f89f6b2c791fb7
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2023-11-15 00:13:02 -0500

    Split Module into header and body files

---
+++ Source/MDMetaDataEditor/Private/MDMetaDataEditorModule.h
@@ -0,0 +1,18 @@
+﻿// Copyright Dylan Dumesnil. All Rights Reserved.
+
+#pragma once
+
+class FMDMetaDataEditorModule : public IModuleInterface
+{
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+	FDelegateHandle VariableCustomizationHandle;
+	FDelegateHandle LocalVariableCustomizationHandle;
+	FDelegateHandle FunctionCustomizationHandle;
+	FDelegateHandle TunnelCustomizationHandle;
+	FDelegateHandle EventCustomizationHandle;
+};
