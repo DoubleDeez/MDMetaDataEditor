edb4f227e74fd3fddd3c6ae088a2f96deb059a5a
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2024-12-06 14:14:07 -0500

    Remove unnecessary include.
    InstancedStruct is included in the .h

--- Source/MDMetaDataEditor/Private/Types/MDMetaDataEditorPropertyType.cpp
+++ Source/MDMetaDataEditor/Private/Types/MDMetaDataEditorPropertyType.cpp
@@ -5,11 +5,6 @@
 
 #include "EdGraph/EdGraphPin.h"
 #include "EdGraphSchema_K2.h"
-#if ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION >= 5
-#include "StructUtils/InstancedStruct.h"
-#else
-#include "InstancedStruct.h"
-#endif
 
 FEdGraphPinType FMDMetaDataEditorPropertyType::ToGraphPinType() const
 {
